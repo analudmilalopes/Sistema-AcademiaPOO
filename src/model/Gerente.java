2d558ea970449b88996f77a85cd7b149729f7f25
Ludlinda <ludmilalopes416@gmail.com>
2026-05-05 10:07:25 -0300

    refactor: Gerente recebe o Service do Aluno

--- src/model/Gerente.java
+++ src/model/Gerente.java
@@ -1,13 +1,13 @@
 package model;
 
 import repository.AlunoRepositoryMemory;
+import service.AlunoService;
 
 public class Gerente extends Pessoa{
 
-    private AlunoRepositoryMemory alunoRepository;
+    private AlunoService alunoService;
 
-    public Gerente(String nome, String cpf, AlunoRepositoryMemory alunoRepository){
+    public Gerente(String nome, String cpf){
         super(nome, cpf);
-        this.alunoRepository = alunoRepository;
     }
 }
