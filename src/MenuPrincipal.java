933ade1df852b5d4846873a6cfffae4a0af7106a
Ludlinda <ludmilalopes416@gmail.com>
2026-05-05 11:18:54 -0300

    fix: Arrumando loop

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -21,15 +21,17 @@ public class MenuPrincipal {
 
     public static String lerNomes(String entradaUsuario) {
 
-        System.out.println(entradaUsuario);
+        while (true) {
+            System.out.println(entradaUsuario);
 
-        String entradaMensagem = scanner.nextLine();
-        if (entradaMensagem.matches("[a-zA-Z ]+")) {
+            String entradaMensagem = scanner.nextLine();
+            if (entradaMensagem.matches("[a-zA-Z ]+")) {
+                return entradaMensagem;
+            } else {
+                System.out.println("Somente letras.");
+            }
             return entradaMensagem;
-        } else {
-            System.out.println("Somente letras.");
         }
-        return entradaMensagem;
     }
 
 
@@ -39,7 +41,7 @@ public class MenuPrincipal {
 
             String entradaMensagem = scanner.nextLine();
 
-            if (!entradaMensagem.matches("\\d{11}")){
+            if (entradaMensagem.matches("\\d{11}")){
                 return entradaMensagem;
             }else {
                 System.out.println("CPF inválido!");
