9d53db59b168ee65398518965edb932e36077cc6
Ludlinda <ludmilalopes416@gmail.com>
2026-05-06 10:59:54 -0300

    feat: Criando tela secundária para mostrar os planos

---
+++ src/TelaSecundaria.java
@@ -0,0 +1,25 @@
+import enums.TipoPlano;
+import model.Plano;
+import service.PlanoService;
+
+import java.util.Scanner;
+
+public class TelaSecundaria {
+
+    Scanner scanner = new Scanner(System.in);
+
+    PlanoService planoService;
+
+    public TelaSecundaria(PlanoService planoService){
+        this.planoService = planoService;
+    }
+
+
+    public void mostrarPlano(TipoPlano tipoPlano){
+        Plano plano = planoService.buscarPorTipoPlano(tipoPlano);
+        System.out.println("--------- " + tipoPlano.name() + " ---------");
+        System.out.println("Duração de " + plano.getDuracaoEmMeses() + " meses");
+        System.out.println("Valor: R$" + plano.getValor() + " reais.");
+        System.out.println("Taxa de matrícula: R$" + plano.getTaxaMatricula() + " reais.");
+    }
+}
