775402e9c7a2f3deb88177ccb77c340a66c8a6c9
Ludlinda <ludmilalopes416@gmail.com>
2026-05-16 12:49:05 -0300

    fix: adicionando loop no gerente para manter o menu ativo

--- src/controller/GerenteController.java
+++ src/controller/GerenteController.java
@@ -19,55 +19,56 @@ public class GerenteController {
 
     public void TelaGerente() {
 
-        int opcoesGerente;
+        while (true) {
+            int opcoesGerente;
 
 
-        do {
+            do {
 
-            opcoesGerente = LeitorEntrada.lerNumerosInteiros("1 - Listar Alunos\n" + "2 - Buscar aluno por ID\n" +
-                    "3 - Buscar aluno por CPF\n" + "4 - Buscar aluno por Nome\n" + "5 - Remover Aluno\n" +
-                    "6 - Buscar aluno por Tipo de Plano\n" + "7 - Sair");
+                opcoesGerente = LeitorEntrada.lerNumerosInteiros("1 - Listar Alunos\n" + "2 - Buscar aluno por ID\n" +
+                        "3 - Buscar aluno por CPF\n" + "4 - Buscar aluno por Nome\n" + "5 - Remover Aluno\n" +
+                        "6 - Buscar aluno por Tipo de Plano\n" + "7 - Sair");
 
-            if (opcoesGerente >= 1 && opcoesGerente <= 7) {
-                break;
-            } else {
-                System.out.println("Escolher entre 1 e 6.");
+                if (opcoesGerente >= 1 && opcoesGerente <= 7) {
+                    break;
+                } else {
+                    System.out.println("Escolher entre 1 e 6.");
+                }
+            } while (opcoesGerente != 0);
+
+            switch (opcoesGerente) {
+                case 1:
+                    alunoService.listarTodosAlunos();
+                    break;
+                case 2:
+                    int idProcurado = LeitorEntrada.lerNumerosInteiros("Digite o ID do aluno que deseja procurar:\n");
+                    mostrarBusca(alunoService.buscarPorId(idProcurado));
+                    break;
+                case 3:
+                    String cpfProcurado = LeitorEntrada.lerCpf("Digite o CPF do aluno que deseja procurar:\n");
+                    mostrarBusca(alunoService.buscarPorCpf(cpfProcurado));
+                    break;
+                case 4:
+                    String nomeProcurado = LeitorEntrada.lerNomes("Digite o Nome do aluno que deseja procurar:\n");
+                    mostrarBusca(alunoService.buscarPorNome(nomeProcurado));
+                    break;
+                case 5:
+                    String cpfRemover = LeitorEntrada.lerCpf("Digite o CPF do aluno que deseja remover:\n");
+                    if (alunoService.removerAlunoPorCpf(cpfRemover)) {
+                        System.out.println("Aluno removido!");
+                    } else {
+                        System.out.println("Aluno não encontrado no sistema!");
+                    }
+                    break;
+                case 6:
+                    String planoProcurado = LeitorEntrada.lerNomes("Digite o nome do Plano que deseja procurar:\n");
+                    mostrarBuscaLista(alunoService.buscarPeloTipoPlano(TipoPlano.valueOf(planoProcurado.toUpperCase())));
+                    break;
+                case 7:
+                   return;
+                default:
+                    System.out.println("Apenas uma das 6 opções são válidas!");
             }
-        } while (opcoesGerente != 0);
-
-        switch (opcoesGerente) {
-            case 1:
-                alunoService.listarTodosAlunos();
-                break;
-            case 2:
-                int idProcurado = LeitorEntrada.lerNumerosInteiros("Digite o ID do aluno que deseja procurar:\n");
-               mostrarBusca(alunoService.buscarPorId(idProcurado));
-                break;
-            case 3:
-                String cpfProcurado = LeitorEntrada.lerCpf("Digite o CPF do aluno que deseja procurar:\n");
-                mostrarBusca(alunoService.buscarPorCpf(cpfProcurado));
-                break;
-            case 4:
-                String nomeProcurado = LeitorEntrada.lerNomes("Digite o Nome do aluno que deseja procurar:\n");
-               mostrarBusca(alunoService.buscarPorNome(nomeProcurado));
-                break;
-            case 5:
-                String cpfRemover = LeitorEntrada.lerCpf("Digite o CPF do aluno que deseja remover:\n");
-               if (alunoService.removerAlunoPorCpf(cpfRemover)){
-                   System.out.println("Aluno removido!");
-               }else {
-                   System.out.println("Aluno não encontrado no sistema!");
-               }
-                break;
-            case 6:
-                String planoProcurado = LeitorEntrada.lerNomes("Digite o nome do Plano que deseja procurar:\n");
-                mostrarBuscaLista(alunoService.buscarPeloTipoPlano(TipoPlano.valueOf(planoProcurado.toUpperCase())));
-                break;
-            case 7:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Apenas uma das 6 opcoes sao validas!");
         }
     }
 
