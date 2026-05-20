d9c342b3fcb75a69abf151bfad5c81bbe6999c2a
Ludlinda <ludmilalopes416@gmail.com>
2026-05-20 11:22:28 -0300

    refactor: altera o salvamento do repositorio para o arquivo

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        AlunoRepository alunoRepo = new AlunoRepositoryMemory();
+        AlunoRepository alunoRepo = new AlunoRepositoryArquivo();
         AlunoService alunoService = new AlunoService(alunoRepo);
 
         PlanoRepository planoRepo = new PlanoRepositoryMemory();
