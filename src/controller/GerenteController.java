f490edb40d871abf09d663c90c04d2b11742d554
Ludlinda <ludmilalopes416@gmail.com>
2026-05-19 21:26:32 -0300

    style: arrumando visualizacao dos dados do aluno

--- src/controller/GerenteController.java
+++ src/controller/GerenteController.java
@@ -74,11 +74,12 @@ public class GerenteController {
 
     public void mostrarBusca(Aluno aluno){;
         if (aluno != null){
-            System.out.println("          👤 DADOS DO ALUNO            ");
+            System.out.println("👤 DADOS DO ALUNO  ");
             System.out.println("ID:   " + aluno.getAlunoID());
             System.out.println("Nome:   " + aluno.getNome());
             System.out.println("CPF:   " + aluno.getCpf());
             System.out.println("Plano:   " + aluno.getPlano().getTipoPlano().name());
+            System.out.println("\n");
         } else {
             System.err.println("Aluno não encontrado!");
         }
