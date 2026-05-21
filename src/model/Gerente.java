101a187303fc75d724471433376e52153767397f
Ludlinda <ludmilalopes416@gmail.com>
2026-05-21 11:52:55 -0300

    refactor: removendo duplicata AlunoService em Gerente

--- src/model/Gerente.java
+++ src/model/Gerente.java
@@ -5,8 +5,6 @@ import service.AlunoService;
 
 public class Gerente extends Pessoa{
 
-    private AlunoService alunoService;
-
     public Gerente(String nome, String cpf){
         super(nome, cpf);
     }
