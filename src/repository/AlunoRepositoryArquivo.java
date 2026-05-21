dfda4a12a27970a09b97640ffb0b6c30a830ef3a
Ludlinda <ludmilalopes416@gmail.com>
2026-05-21 16:36:29 -0300

    feat: implementando listar todos e remover aluno

--- src/repository/AlunoRepositoryArquivo.java
+++ src/repository/AlunoRepositoryArquivo.java
@@ -1,27 +1,32 @@
 package repository;
 
+import enums.TipoPlano;
 import model.Aluno;
+import model.Plano;
+import service.PlanoService;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlunoRepositoryArquivo implements AlunoRepository{
 
     private final String arquivo = "alunos.txt";
+    private final PlanoService planoService;
 
+    public AlunoRepositoryArquivo(PlanoService planoService){
+        this.planoService = planoService;
+    }
     @Override
     public void cadastrarAluno(Aluno aluno) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo, true))){
 
             writer.write(
-                    aluno.getAlunoID() + ";" +
-                            aluno.getNome() + ";" +
-                            aluno.getCpf() + ";" +
-                            aluno.getPlano().getTipoPlano().name() + ";" +
-                            aluno.getPeso() + ";" +
+                    aluno.getAlunoID() + "," +
+                            aluno.getNome() + "," +
+                            aluno.getCpf() + "," +
+                            aluno.getPlano().getTipoPlano().name() + "," +
+                            aluno.getPeso() + "," +
                             aluno.getAltura()
             );
 
@@ -33,11 +38,57 @@ public class AlunoRepositoryArquivo implements AlunoRepository{
 
     @Override
     public List<Aluno> listarTodos() {
-        return new ArrayList<>();
+        List<Aluno> alunos = new ArrayList<>();
+
+        try(BufferedReader reader =  new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while ((linha = reader.readLine()) != null){
+                String[] campos = linha.split(",");
+
+                int id = Integer.parseInt(campos[0]);
+                String nome = campos[1];
+                String cpf = campos[2];
+                TipoPlano tipoPlano = TipoPlano.valueOf(campos[3]);
+                double peso = Double.parseDouble(campos[4]);
+                double altura = Double.parseDouble(campos[5]);
+
+                Plano plano = planoService.buscarPorTipoPlano(tipoPlano);
+                Aluno aluno = new Aluno(nome, cpf, plano, peso, altura);
+                aluno.setAlunoID(id);
+                alunos.add(aluno);
+            }
+        }catch (IOException e){
+            System.err.println("Erro ao ler arquivo de alunos!");
+        }
+
+        return alunos;
     }
 
     @Override
     public boolean removerAluno(String cpf) {
-        return false;
+        List<Aluno> alunos = listarTodos();
+        boolean encontrouAlunos = alunos.removeIf(aluno -> aluno.getCpf().equals(cpf));
+
+        if (!encontrouAlunos) {
+            return false;
+        }                                                                // sobrescrever o arquivo para n duplicar
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo, false))) {
+            for (Aluno aluno : alunos){
+                writer.write(
+                        aluno.getAlunoID() + "," +
+                                aluno.getNome() + "," +
+                                aluno.getCpf() + "," +
+                                aluno.getPlano().getTipoPlano().name() + "," +
+                                aluno.getPeso() + "," +
+                                aluno.getAltura()
+                );
+                writer.newLine();
+            }
+        } catch (IOException e){
+            System.err.println("Erro ao atualizar arquivo de alunos!");
+        }
+
+        return true;
     }
+
 }
